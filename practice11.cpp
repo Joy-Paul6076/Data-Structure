957e78346c00e3770c8e64f6f7243b18fba2d208
Joy-Paul6076 <joypauljp102@gmail.com>
2026-04-09 01:02:28 +0600

    adding

---
+++ practice11.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+// Factorial of prime numbers
+bool is_prime(int n){
+    if(n<=1){
+        return false;
+    }
+    if (n<=3){
+        return true;
+    }
+    if (n%2==0 || n%3==0){
+        return false;
+    }
+    for(int i=5; i*i<=n; i+=6){
+        if(n%i==0 || n%(i+2)==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(void){
+   int n,x=1;
+   cout<<"Enter a number: ";
+   cin>>n;
+    if(is_prime(n)){
+        for(int i=1; i<=n; ++i){
+        x = x*i;
+        }
+        cout<<"The Factorilal of "<<n<<" is: "<<x<<endl;
+    }
+    else{
+        cout<<n<<" is not a prime number."<<endl;
+    }
+    return 0;
+}
