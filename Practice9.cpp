624ba01da6de535cb2cd2a8b02837f9d91786b77
Joy-Paul6076 <joypauljp102@gmail.com>
2026-04-08 21:32:31 +0600

    Adding practice

--- Practice9.cpp
+++ Practice9.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     string s;
+    cout<<"Enter a string: ";
     getline(cin,s);
     cout<<"String object accessed using array and loop...\n";
     for(int i=0; i<s.length(); i++){
